53e4507dc850167128629725f27e9868b2520570
Victor <victor.bagrin.md@gmail.com>
2026-03-03 17:26:53 -0800

    Implemented client request sending logic

--- interprocess/client.c
+++ interprocess/client.c
@@ -2,8 +2,8 @@
  * Operating Systems  (2INCO)  Practical Assignment
  * Interprocess Communication
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
- * STUDENT_NAME_2 (STUDENT_NR_2)
+ * Victor Bagrin (2011204)
+ * 
  *
  * Grading:
  * Your work will be evaluated based on the following criteria:
@@ -40,5 +40,57 @@ int main (int argc, char * argv[])
     //    until there are no more requests to send
     //  * close the message queue
     
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <REQ_QUEUE_NAME>\n", argv[0]);
+        return 1;
+    }
+
+    const char *req_qname = argv[1];
+
+    // Openning the request queue
+    mqd_t req_mq = mq_open(req_qname, O_WRONLY);
+    if (req_mq == (mqd_t)-1) {
+        perror("client: mq_open(req)");
+        return 1;
+    }
+
+    #ifdef getattr
+    getattr(req_mq);
+    #endif
+
+    while (1) {
+        int jobID = 0, data = 0, serviceID = 0;
+        int rc = getNextRequest(&jobID, &data, &serviceID);
+
+        if (rc == NO_REQ) {
+            // No more requests to send
+            break;
+        }
+        if (rc != NO_ERR) {
+            fprintf(stderr, "client: getNextRequest returned error %d\n", rc);
+            mq_close(req_mq);
+            return 1;
+        }
+
+        ipc_msg_t msg;
+        msg.kind       = IPC_MSG_REQ;
+        msg.job_id     = jobID;
+        msg.service_id = serviceID;
+        msg.data       = data;
+        msg.result     = 0;
+        msg._reserved  = 0;
+
+        if (mq_send(req_mq, (const char *)&msg, sizeof(ipc_msg_t), 0) == -1) {
+            perror("client: mq_send(req)");
+            mq_close(req_mq);
+            return 1;
+        }
+    }
+
+    if (mq_close(req_mq) == -1) {
+        perror("client: mq_close(req)");
+        return 1;
+    }
+
     return (0);
 }
